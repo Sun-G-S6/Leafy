ebf9edd30a5236777e66d6b4b6a07a9ccf55bee4
Niti T <77049172+Sun-G-S6@users.noreply.github.com>
2023-04-12 01:30:28 -0700

    made register and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,25 @@ import './App.css'
 import {Route,Routes} from "react-router-dom";
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+import AboutPage from './pages/AboutPage';
+import HowPage from './pages/HowPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/how" element={<HowPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+      
     </Routes>
     
   )
 }
 
 export default App
-
-//19:01
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 
-export default function LoginPage() {
-    return(
+export default function RegisterPage() {
+    return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form className="max-w-lg mx-auto ">
+                    <input type="fName" placeholder="First Name" />
+                    <input type="lName" placeholder="Last Name" />
                     <input type="email" placeholder="your@email.com" />
                     <input type="password" placeholder="password" />
                     <button className="primary">Login</button>
                     <div className="text-center py-2 text-gray-500">
-                        Sign up <Link className="underline text-bn" to={'/register'}> Here</Link>
+                        Already have an account? <Link className="underline text-bn" to={'/login'}>Login Here</Link>
                     </div>
                 </form>
             </div>
