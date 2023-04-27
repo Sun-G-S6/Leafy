19b3b385eda7d56acedc0e30a385513c33aafe9d
Spartan <spartan@IMS-128MBA.local>
2023-04-26 18:21:15 -0700

    Added a working picture Carrousel

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,7 +1,11 @@
 export default function AboutPage() {
     return (
         <div>
+            
             About page here
+
+            Add a carrousel here
+        
         </div>
     );
 }
