f9ef3dfacf41ba7ae65551b970441dad8079c268
dyashik <yashikdhanaraj@gmail.com>
2023-05-07 13:01:48 -0700

    adds to cart correctly, removes from cart, clears cart, and fixes some minor bugs (quantity reset + syntax errors)

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -11,7 +11,8 @@ import orange from '../pictures/orange.jpg';
 import pear from '../pictures/pear.jpg';
 import pineapple from '../pictures/pineapple.jpg';
 import strawberry from '../pictures/strawberry.jpg';
-import cart from '../pictures/cart.jpg'
+import remove from '../pictures/delete.png';
+// import cart from '../pictures/cart.jpg'
 
 export default function SearchPage() {
     const dummyProducts = [
@@ -28,57 +29,75 @@ export default function SearchPage() {
         { name: "Pineapple", price: "$3.99", imgSrc: pineapple, seller: "Healthy Foods", distance: "2.3 miles", quantity: 2 },
         { name: "Strawberry", price: "$1.99", imgSrc: strawberry, seller: "Mary's Market", distance: "0.8 miles", quantity: 3 },
     ];
-      
 
+    // const cart = [
+    //     { name: "Strawberry", price: "$1.99", imgSrc: strawberry, seller: "Mary's Market", distance: "0.8 miles", quantity: 3 }
+    // ];
+    
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState(dummyProducts);
-    const [cart, setCart] = useState([]);
     const [quantity, setQuantity] = useState(1);
+
     const handleChange = event => {
         setSearchTerm(event.target.value);
     };
+    
 
     const handleSubmit = (event) => {
         event.preventDefault();
         let results = [];
         if (searchTerm.trim() === "") {
-        results = dummyProducts; // Display all products when search term is empty
+            results = dummyProducts; // Display all products when search term is empty
         } else {
             results = dummyProducts.filter((product) =>
-            product.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
+                product.name.toLowerCase().includes(searchTerm.toLowerCase())
+            );
         }
         setSearchResults(results);
     };
 
   
 
-    // const addToCart = (product) => {
-    //     setCart([...cart, product]);
-    // };
+    const [cart, setCart] = useState([]);
+    const [cartLength, setCartLength] = useState(0);
 
-    const addToCart = (product) => {
-        const existingProductIndex = cart.findIndex(
-          (item) => item.name === product.name
-        );
-        if (existingProductIndex !== -1) {
-          const updatedCart = [...cart];
-          const existingProduct = { ...updatedCart[existingProductIndex] };
-          existingProduct.quantity += 1;
-          updatedCart[existingProductIndex] = existingProduct;
-          setCart(updatedCart);
+    const clearCart = (event) => {
+        event.preventDefault();
+        setCart([]);
+        setCartLength(0);
+    }
+
+    const removeCart = (event, product) => {
+        event.preventDefault(); // prevent the default behavior of the img element (e.g. opening the image in a new tab)
+        const newCart = cart.filter((p) => p !== product); // create a new array of products that don't match the clicked product
+        setCartLength(cartLength - product.quantity);
+        setCart(newCart); // update the cart state with the new array
+    }
+
+    const addToCart = (event, product, quantity) => {
+        event.preventDefault();
+        const existingItemIndex = cart.findIndex((item) => item.name === product.name);
+        
+        if (existingItemIndex !== -1) {
+            const updatedCartItems = [...cart];
+            updatedCartItems[existingItemIndex] = {
+                ...updatedCartItems[existingItemIndex],
+                quantity: updatedCartItems[existingItemIndex].quantity + quantity,
+            };
+            setCart(updatedCartItems);
         } else {
-          const newProduct = { ...product, quantity: 1 };
-          setCart([...cart, newProduct]);
+            setCart([...cart, { ...product, quantity: quantity }]);
         }
-      };
+        setCartLength(cartLength + quantity);
+        setQuantity(1);
+    };
+      
       
 
-    
 
     const handleIncrease = () => {
         setQuantity(quantity + 1);
-      };
+    };
     
     const handleDecrease = () => {
         if (quantity > 1) {
@@ -90,14 +109,22 @@ export default function SearchPage() {
     <div>
       <div>
         {/* <h1>Search Page</h1> */}
-        
-        
         <form onSubmit={handleSubmit}>
             <div>
                 <div style={{ display: "flex", justifyContent: "center", paddingTop: "20px" }}>
-                    <a onClick={() => addToCart(product)} className="d-block text-center">
-                        <img src="src/pictures/cart.jpg" alt="Add to Cart" style={{ width: "50px", height: "50px" }} />
-                        <h10>In Cart: {cart.filter(item => item.id === product.id).length}</h10>
+                    <a className="d-block text-center">
+                        <div onClick={(event) => clearCart(event)} style={{ display: "flex", justifyContent: "center", paddingTop: "20px" }}>
+                            <img src="src/pictures/cart.jpg" alt="Add to Cart" style={{ width: "50px", height: "50px" }} />
+                        </div>
+                        <h10>Cart: {cartLength}</h10>
+                        <div>
+                            {cart.map((product) => (
+                                <div onClick={(event) => removeCart(event, product)} key={product.name} style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                    <p>{`${product.name} (${product.quantity})`}</p>
+                                    <img src={remove} style={{ paddingLeft: "10px" , paddingTop: "3px"}} width="4%" height="4%" />
+                                </div>
+                            ))}
+                        </div>
                     </a>
                 </div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
@@ -106,29 +133,28 @@ export default function SearchPage() {
                     </label>
                 </div>
             </div>
-          
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <div>
-              <button
-                type="submit"
-                style={{
-                    backgroundColor: "#4CAF50",
-                    border: "none",
-                    color: "white",
-                    padding: "10px 20px",
-                    textAlign: "center",
-                    textDecoration: "none",
-                    display: "inline-block",
-                    fontSize: "16px",
-                    margin: "4px 2px",
-                    cursor: "pointer",
-                    borderRadius: "5px",
-                }}
-              >
-                Submit
-              </button>
+        
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div>
+                    <button
+                        type="submit"
+                        style={{
+                            backgroundColor: "#4CAF50",
+                            border: "none",
+                            color: "white",
+                            padding: "10px 20px",
+                            textAlign: "center",
+                            textDecoration: "none",
+                            fontSize: "16px",
+                            margin: "4px 2px",
+                            cursor: "pointer",
+                            borderRadius: "5px",
+                        }}
+                    >
+                        Submit
+                    </button>
+                </div>
             </div>
-          </div>
         </form>
         
       </div>
@@ -143,9 +169,9 @@ export default function SearchPage() {
         {searchResults.length > 0 && (
             <div
                 style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
-                gap: "1rem",
+                    display: "grid",
+                    gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+                    gap: "1rem",
                 }}
             >
             {searchResults.map((product) => (
@@ -170,50 +196,52 @@ export default function SearchPage() {
                   <p>{product.price}</p>
                   <p>{product.distance}</p>
                 </div>
-                <div style={{ display: "flex", justifyContent: "center" , paddingBottom: "10px"}}>
-                    <button
-                        
-                        style={{
-                            backgroundColor: "#4CAF50",
-                            border: "none",
-                            color: "white",
-                            padding: "10px 20px",
-                            textAlign: "center",
-                            textDecoration: "none",
-                            display: "inline-block",
-                            fontSize: "16px",
-                            margin: "4px 2px",
-                            cursor: "pointer",
-                            borderRadius: "5px",
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            minWidth: "120px",
-                        }}
-                    >
-                        <span onClick={() => addToCart(product)} style={{ marginRight: "10px" }}>Add to cart</span>
-                        <div
+                    <div style={{ display: "flex", justifyContent: "center" , paddingBottom: "10px"}}>
+                        <button
+                            
                             style={{
+                                backgroundColor: "#4CAF50",
+                                border: "none",
+                                color: "white",
+                                padding: "10px 20px",
+                                textAlign: "center",
+                                textDecoration: "none",
+                                fontSize: "16px",
+                                margin: "4px 2px",
+                                cursor: "pointer",
+                                borderRadius: "5px",
                                 display: "flex",
                                 alignItems: "center",
-                                backgroundColor: "white",
-                                color: "#4CAF50", 
-                                width: "20px",
-                                height: "30px",
-                                borderRadius: "5px",
                                 justifyContent: "center",
-                                marginLeft: "10px",
+                                minWidth: "120px",
                             }}
                         >
-                            <div style={{ display: "flex", alignItems: "center" }}>
-                                <button onClick={handleDecrease} style={{ marginRight: "11px", color: 'white' }}>-</button>
-                                <span style={{ margin: "0px" }}>{quantity}</span>
-                                <button onClick={handleIncrease} style={{ marginLeft: "11px", color: 'white' }}>+</button>
+                            <div>
+                                <span onClick={(event) => addToCart(event, product, quantity)} style={{ marginRight: "10px" }}> Add to cart </span>
                             </div>
-                        </div>
-                    </button>
+                            
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    backgroundColor: "white",
+                                    color: "#4CAF50", 
+                                    width: "20px",
+                                    height: "30px",
+                                    borderRadius: "5px",
+                                    justifyContent: "center",
+                                    marginLeft: "10px",
+                                }}
+                            >
+                                <div style={{ display: "flex", alignItems: "center" }}>
+                                    <button onClick={handleDecrease} style={{ marginRight: "11px", color: 'white' }}>-</button>
+                                    <span style={{ margin: "0px" }}>{quantity}</span>
+                                    <button onClick={handleIncrease} style={{ marginLeft: "11px", color: 'white' }}>+</button>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
                 </div>
-              </div>
             ))}
           </div>
         )}
