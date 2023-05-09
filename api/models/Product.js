a0ffe60ae48ef8a38a7d6b9f3de1d1885b81d8c1
Niti T <77049172+Sun-G-S6@users.noreply.github.com>
2023-05-09 01:18:41 -0700

    Added product model to the database

--- api/models/Product.js
+++ api/models/Product.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     owner: {type:mongoose.Schema.Types.ObjectId, ref:'User'},
     name: String,
+    price: Number,
+    quantity: Number,
     address: String,
     photos: [String],
     description: String,
+    category: [String],
 });
 
 const ProductModel = mongoose.model('Product', productSchema);
