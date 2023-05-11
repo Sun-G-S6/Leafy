66ed8a2c5f174055a9d98ebc578269ed5f3fb3a4
Spartan <spartan@IMS-128MBA.local>
2023-05-10 18:49:44 -0700

    New Index Page

---
+++ client/src/components/table.jsx
@@ -0,0 +1,27 @@
+/**import React from "react";
+
+const table = () => {
+return (
+    <div className="table">
+        <table>
+          <thead>
+            <th>
+                Header 1
+            </th>
+
+            <th>
+                Header 2
+            </th>
+
+
+            <th>
+                Header 3
+            </th>
+
+
+          </thead>
+        </table>
+      </div>
+);
+}**/
+
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,7 +1,57 @@
+import React from "react";
+import Carousel from "../components/Carousel.jsx";
+//import Table from "../components/table.jsx";
+
+const slides = [
+  "https://www.racv.com.au/content/dam/racv/images/content-hub/home-property/gardening/how-to-grow-a-vegetable-patch/1600x600GettyImages-532865778.jpg.transform/heroMob/image.jpg",
+  "https://www.racv.com.au/content/dam/racv/images/content-hub/home-property/gardening/how-to-grow-a-vegetable-patch/1600x600GettyImages-532865778.jpg.transform/heroMob/image.jpg",
+  "https://www.gannett-cdn.com/-mm-/129bc326106e4ea50bc2e46b1892292c00453fe3/c=0-217-4262-2625/local/-/media/2017/05/03/Westchester/Westchester/636294139087548295-GettyImages-613517018-1-.jpg",
+  "https://www.farmersalmanac.com/wp-content/uploads/2020/11/garden-harvest-produce-last-longer-As_305375846.jpeg"
+]
+
 export default function IndexPage() {
     return(
-        <div>
-            Index page here
+      <main className = "App">
+        <div className="max-w-lg">
+          <Carousel autoSlide = {true}>
+            {slides.map((s) => (
+              <img src = {s} />
+            ))}
+          </Carousel>
         </div>
+
+
+        <div style ={{ textAlign: "center" }}>
+        <table>
+          <caption> //------Why Leafy? --------// </caption>
+          
+          <thead>
+            <tr>
+              <th>    -----BUILD COMMUNITY-----    </th>
+              
+              <th>     ------COST & TIME EFFECTIVE-----    </th>
+
+              <th>     ----SUSTAINABLE  MISSION--------   </th>
+
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Add picture</td>
+              <td>Add picture </td>
+              <td>Add picture</td>
+            </tr>
+            <tr>
+              <td>|| Promotes interactions amongst neighbors || </td>
+              <td>|| Can help save money & time with its ease of access in the vicinity ||</td>
+              <td>|| Helping conserve existing native trees and plants || </td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+
+      </main>
+
+      
     );
 }
