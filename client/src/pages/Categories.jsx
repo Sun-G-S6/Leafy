a1fbdc7841d2f99409d9aec49ad0176f4cb1c891
Niti T <77049172+Sun-G-S6@users.noreply.github.com>
2023-05-12 02:41:17 -0700

    fixed console issues with svgs

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,12 +1,21 @@
 export default function Categories({selected, onChange}) {
+    function handleCbClick(ev) {
+        const {checked, name} = ev.target;
+        if(checked) {
+            onChange([...selected, name]);
+        } else {
+            onChange([...selected.filter(selectedName => selectedName !== name)]);
+        }
+        
+    }
     return(
         <>
             <label className="flex border rounded-2xl p-4 gap-2 items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name="fruits" onChange={handleCbClick} />
                 <span>Fruits</span>
             </label>
             <label className="flex border rounded-2xl p-4 gap-2 items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name="vegetables" onChange={handleCbClick} />
                 <span>Vegetables</span>
             </label>
         </>
